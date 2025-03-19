63f1c8d9ff08b476b7d1f85701f63801cc2d9143
Brian Ott <bott@b1-systems.de>
2025-03-19 09:00:45 +0100

    add: refactoring main.py

---
+++ stats.py
@@ -0,0 +1,14 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
